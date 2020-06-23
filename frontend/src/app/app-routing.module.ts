818ca56ec87d2952285cb01c4e96682a348ef5d1
Magnaldo Melo <magnaldo.melo@gmail.com>
2020-06-22 21:27:06 -0300

    Frontend - Add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequrestResetComponent } from './components/password/requrest-reset/requrest-reset.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'request-password-reset', component: RequrestResetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
